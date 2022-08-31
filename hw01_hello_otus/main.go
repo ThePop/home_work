3d07a98cf0ac55a4a74d0bd55db418dc2212c909
PavelVlasov <pavel.vlasov@vseinstrumenti.ru>
2022-08-31 19:40:51 +0300

    fix lint

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+
 	"golang.org/x/example/stringutil"
 )
 
 func main() {
-	sourceStr := "Hello, OTUS!";
+	sourceStr := "Hello, OTUS!"
 	fmt.Println(stringutil.Reverse(sourceStr))
 }
